13804db29e86f957556371646d9aa29065a2c89f
Kirthana14 <keerthiroses123@gmail.com>
2018-03-01 09:03:59 +0530

    init

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Throwable {
+        //Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
